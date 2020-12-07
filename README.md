401141e41812b1e2306b12fd7860891dc655b67b
panditaditi07 <aditi.pandit@raweng.com>
2020-12-07 21:51:20 +0530

    added files

---
+++ README.md
@@ -0,0 +1,4 @@
+## This is Blog Backend through express js
+
+1. This gets all the users.
+2. It also gets one user using the user's ID.
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+const PORT = 3000;
+const USERS = path.join(__dirname, "data", "users.json");
+const userData = JSON.parse(fs.readFileSync(USERS, "utf-8"));
+app.get("/users", (req, res) => {
+  res.send(userData);
+});
+
+app.get("/users/:id", (req, res) => {
+  let user = userData.find((user) => {
+    return user.id === req.params.id;
+  });
+  if (user) {
+    res.status(200).json({
+      status: "successful",
+      data: user,
+    });
+  } else {
+    res.status(200).json({
+      status: "User not found",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server running at ${PORT}`);
+});
