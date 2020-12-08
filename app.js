cf72a7e9c8ebfee00602282b24a52816201bb2d4
panditaditi07 <aditi.pandit@raweng.com>
2020-12-08 10:02:56 +0530

    changed to json

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const USERS = path.join(__dirname, "data", "users.json");
 const userData = JSON.parse(fs.readFileSync(USERS, "utf-8"));
 app.get("/users", (req, res) => {
-  res.send(userData);
+  res.send.json(userData);
 });
 
 app.get("/users/:id", (req, res) => {
