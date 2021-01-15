e9fa8047ad1163c83d91912f1c60538b3eddd910
panditaditi07 <aditi.pandit@raweng.com>
2021-01-15 09:32:06 +0530

    added port 5000
    installed cors and added cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
@@ -10,6 +10,6 @@ const blogRouter = require("./routes/userRoutes");
 app.use(express.json());
 app.use("/blogs", blogRouter);
 
-app.listen(5000, () => {
-  console.log("server running on PORT 5000");
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
 });
